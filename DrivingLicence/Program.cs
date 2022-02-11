0f929c3c84f94d3884b92372c6daf48fbc3b471d
Hele-Mai Ristmäe <hristmae@tktk.ee>
2022-02-11 21:46:28 +0200

    Control Flow

---
+++ DrivingLicence/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DrivingLicence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutaja sünniaastat
+            //Programm arvutab kasutaja vanust
+            //Kui kasutaja on noorem kui 18, siis programm kuvab konsoolis
+            //"Oled liiga noor, et juhilube saada."
+            //Kui kasutaja on vanem kui 18, siis programm kuvab konsoolis
+            //"Oled piisavalt vana, et juhilube saada."
+            //Kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd sa saad ka juhilube taotleda!"
+
+            Console.WriteLine("Palun, sisestage oma sünniaasta:");
+
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearOfBirth;
+
+            if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada.");
+            }
+
+            else if (userAge < 18)
+            {
+             Console.WriteLine("Oled liiga noor, et juhilube saada.");
+            }
+
+            else
+            {
+                Console.WriteLine("Palju õnne! Nüüd saad ka juhilube taotleda!");
+            }
+                
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programm küsib kasutaja vanust;
+            //Pragramm arvutab kasutaja sünnaastat;
+            //Programm kuvab sünniaastat konsoolis;
+            //Oled sündinud aastal {yearOfBirth}
+
+            Console.WriteLine("Palun, sisestage oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2022 - userAge;
+
+            Console.WriteLine($"Oled sündinud {yearOfBirth} aastal.");
+
+            
+
+
+
+
+        }
+    }
+}
